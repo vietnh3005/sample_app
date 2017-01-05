a9e3f2698de032a492d3831ffb018a01eae3f0fb
vietnh3005 <vietnh3005@gmail.com>
2017-01-05 16:01:21 +0700

    Filling layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,5 @@ module ApplicationHelper
   def full_title page_title = ""
     base_title = "Ruby on Rails Tutorial Sample App"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
-    end
   end
 end
