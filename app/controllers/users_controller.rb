d251a057819be5dff454e059b98e2b03cd445693
vietnh3005 <vietnh3005@gmail.com>
2017-01-07 13:01:59 +0700

    Implement basic log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find_by id params[:id]
+    @user = User.find_by id: params[:id]
     if @user.nil?
-      flash.alert = "User does not exist!!!"
-      redirect_to root_path
+      flash[:danger] = "User does not exist!!!"
+      redirect_to signup_path
     end
   end
 
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- test/helpers/application_helper_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title("Contact"),"Contact | Ruby on Rails Tutorial Sample App"
-  end
-end
