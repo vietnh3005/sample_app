98ed6edd0ec5c08548072e8d953459a836ad3f2b
vietnh3005 <vietnh3005@gmail.com>
2017-01-10 09:33:09 +0700

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -6,19 +6,20 @@
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# you"ll amass, the slower it"ll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It"s strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107061047) do
+ActiveRecord::Schema.define(version: 20170109070717) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
